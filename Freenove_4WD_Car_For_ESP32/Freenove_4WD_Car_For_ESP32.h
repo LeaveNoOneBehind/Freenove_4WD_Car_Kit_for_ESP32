ad1aa89670dd54ad90ce7529fb7b9e7cd3fd4eba
redn1nja <ostap.seryvko@ucu.edu.ua>
2024-08-08 15:47:58 +0300

    added master-header

---
+++ Freenove_4WD_Car_For_ESP32/Freenove_4WD_Car_For_ESP32.h
@@ -0,0 +1,15 @@
+#ifndef FREENOVE_4WD_CAR_FOR_ESP32_H
+#define FREENOVE_4WD_CAR_FOR_ESP32_H
+
+#include "Freenove_Battery.h"
+#include "Freenove_Buzzer.h"
+#include "Freenove_Camera.h"
+#include "Freenove_Emotion.h"
+#include "Freenove_LED.h"
+#include "Freenove_PCA9695.h"
+#include "Freenove_Photosensitive.h"
+#include "Freenove_Track.h"
+#include "Freenove_Ultrasonic.h"
+#include "Freenove_WiFi.h"
+
+#endif //FREENOVE_4WD_CAR_FOR_ESP32_H
